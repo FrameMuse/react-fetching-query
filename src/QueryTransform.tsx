2f684980843e0b86f4d09634a6f65cd3c57fa3b4
FrameMuse <minicablestone@gmail.com>
2021-09-02 14:21:27 +0300

    QueryTransform add

---
+++ src/QueryTransform.tsx
@@ -0,0 +1,31 @@
+import { createContext } from "react"
+import { useQueryContext } from "QueryContext"
+
+interface QueryTransformProps {
+  children: any
+  transform(props: { children: any }): JSX.Element
+}
+
+export const QueryTransformContext = createContext<QueryTransformProps["transform"]>(DefaultQueryTransformation)
+
+export function QueryTransform(props: QueryTransformProps) {
+  return (
+    <QueryTransformContext.Provider value={props.transform}>
+      {props.children}
+    </QueryTransformContext.Provider>
+  )
+}
+
+function DefaultQueryTransformation(props: { children: any }) {
+  const queryContext = useQueryContext()
+
+  if (queryContext.loading) {
+    return null
+  }
+
+  if (queryContext.errorObject instanceof Error) {
+    return queryContext.errorObject.message
+  }
+
+  return props.children
+}
